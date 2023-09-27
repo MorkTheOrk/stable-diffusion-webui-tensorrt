95c35be6965e0e88bf9fe8177816189304b23aa0
Luca <lspindler@nvidia.com>
2023-09-27 13:57:16 +0200

    fix trt install

--- install.py
+++ install.py
@@ -5,9 +5,7 @@ from logging import info
 def install():
     if not launch.is_installed("tensorrt"):
         info("TensorRT is not installed! Installing...")
-        launch.run_pip(f"install nvidia-cudnn-cu11==8.9.4.25", "nvidia-cudnn-cu11")
         launch.run_pip(f"install --pre --extra-index-url https://pypi.nvidia.com tensorrt==9.0.1.post11.dev4", "tensorrt")
-        launch.run(['python','-m','pip','uninstall','-y','nvidia-cudnn-cu11'], "removing nvidia-cudnn-cu11")
 
     if not launch.is_installed("onnx"):
         info("ONNX is not installed! Installing...")
