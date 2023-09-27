6e44ee332adf81f7446fa985cacb53f3a2520992
Luca <lspindler@nvidia.com>
2023-09-27 15:17:54 +0200

    enable stdout

--- install.py
+++ install.py
@@ -1,26 +1,22 @@
-import os
 import launch
-from logging import info
 
 def install():
     if not launch.is_installed("tensorrt"):
-        info("TensorRT is not installed! Installing...")
-        launch.run_pip(f"install --pre --extra-index-url https://pypi.nvidia.com tensorrt==9.0.1.post11.dev4", "tensorrt")
+        print("TensorRT is not installed! Installing...")
+        launch.run_pip("install --pre --extra-index-url https://pypi.nvidia.com tensorrt==9.0.1.post11.dev4 --no-deps --no-cache-dir", "tensorrt", True)
 
     if not launch.is_installed("onnx"):
-        info("ONNX is not installed! Installing...")
-        launch.run_pip(f"install onnx")
+        print("ONNX is not installed! Installing...")
+        launch.run_pip("install onnx")
 
     # Polygraphy	
     if not launch.is_installed("polygraphy"):
-        info("Polygraphy is not installed! Installing...")
-        launch.run_pip(f'install polygraphy --extra-index-url https://pypi.ngc.nvidia.com', "polygraphy")
+        print("Polygraphy is not installed! Installing...")
+        launch.run_pip("install polygraphy --extra-index-url https://pypi.ngc.nvidia.com", "polygraphy", True)
 
     # ONNX GS
     if not launch.is_installed("onnx-graphsurgeon"):
-        info("GS is not installed! Installing...")
-        launch.run_pip(f'install onnx-graphsurgeon --extra-index-url https://pypi.ngc.nvidia.com', "onnx-graphsurgeon")
-
-    # TODO optional 
-    # onnxruntime==1.15
+        print("GS is not installed! Installing...")
+        launch.run_pip("install onnx-graphsurgeon --extra-index-url https://pypi.ngc.nvidia.com", "onnx-graphsurgeon", True)
+        
 install()
